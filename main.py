9f68436a8ff4c36de600c9bae71e82cd51787e6a
vivekworldindia <vivekchoudharyup9120@gmail.com>
2025-09-23 06:33:02 +0530

    this

---
+++ main.py
@@ -0,0 +1,24 @@
+# python varialles and functions 
+
+
+# = assignment operator
+a= 20 # this data type in interger
+b = 10 # this data type in integer
+name = "vivek" # this data type is string
+is_true = True # this data type is boolean
+student_name = ["vavek","ajay","sachin"] # this data type is list
+student_marks = (90,80,70) #this data type is tuple
+student_info = {"name":"vivek","age":19} #this data type is dictionary
+student_age = {19,20,21} # this data type is set
+student_car = None #this data type is NoneType
+
+# check data type 
+print(type(is_true))
+
+# arthmetic oprertors
+
+print(a+b) #addition # answer is 30 integer
+
+# check data type
+
+print(type(a))
--- name.py
+++
@@ -1,6 +0,0 @@
-# name is this variable 
-
-name = "vivek"
-age = 19
-print(age)
-print(name)
